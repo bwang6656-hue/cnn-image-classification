9a754615f8948229a7109aa9e9b636de78df0408
智慧贝极星 <16612373+bk07171@user.noreply.gitee.com>
2026-03-05 16:15:54 +0800

    ok

--- plot_compare_curve.py
+++ plot_compare_curve.py
@@ -4,14 +4,19 @@ import matplotlib.pyplot as plt
 with open("resnet18_curve.json", "r", encoding="utf-8") as f:
     resnet = json.load(f)
 
+with open("resnet18_curve_2.json", "r", encoding="utf-8") as f:
+    resnet_cifar = json.load(f)
+
 with open("simplecnn_curve.json", "r", encoding="utf-8") as f:
     cnn = json.load(f)
 
 epochs_resnet = range(1, len(resnet["test_accs"]) + 1)
+epochs_resnet_cifar = range(1, len(resnet_cifar["test_accs"]) + 1)
 epochs_cnn = range(1, len(cnn["test_accs"]) + 1)
 
 plt.figure()
 plt.plot(epochs_resnet, resnet["test_accs"], label="ResNet18 (Transfer Learning)")
+plt.plot(epochs_resnet_cifar, resnet_cifar["test_accs"], label="ResNet18 (From Scratch)")
 plt.plot(epochs_cnn, cnn["test_accs"], label="Simple CNN (From Scratch)")
 plt.xlabel("Epoch")
 plt.ylabel("Test Accuracy (%)")
